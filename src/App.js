4e273ebbd210e6307b17a262dda33bc2695e4483
Ruslan K <ruslan.kuyanets@yandex.ru>
2023-05-10 20:31:04 +0300

    add useOnlineStatus

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Header from "./components/Header/Header";
+import NoInternet from "./components/NoInternet/NoInternet";
 import { Router } from "./routes/Router";
+import { useOnlineStatus } from "./hooks/useOnlineStatus";
 
 function App() {
+  const isOnline = useOnlineStatus();
+
+  if (!isOnline) return <NoInternet />;
+
   return (
     <div className="App">
       <Header />
---
+++ src/components/NoInternet/NoInternet.jsx
@@ -0,0 +1,7 @@
+import style from "./style.module.css";
+
+const NoInternet = () => {
+  return <div className={style.noInternet}>...No internet connection</div>;
+};
+
+export default NoInternet;
