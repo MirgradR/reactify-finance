8691a2da1e43ee4a4bd0e8c39277817ef4128b31
Ruslan K <ruslan.kuyanets@yandex.ru>
2023-05-10 19:58:08 +0300

    add hook useDebounce

--- src/hooks/useFilterStocks.js
+++ src/hooks/useFilterStocks.js
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import { filterStocks } from "../helpers/filterStocks";
+import { useDebounce } from "./useDebounce";
 
 export const useFilterStocks = (data) => {
   const [value, setValue] = useState("");
   const [stocks, setStocks] = useState([]);
 
+  const debouncedValue = useDebounce(value, 1500);
+
   useEffect(() => {
     if (!data) return;
-    const filteredStocks = filterStocks(data, value);
+    const filteredStocks = filterStocks(data, debouncedValue);
     setStocks(filteredStocks);
-  }, [data, value]);
+  }, [data, debouncedValue]);
 
   return { setValue, value, stocks };
 };
